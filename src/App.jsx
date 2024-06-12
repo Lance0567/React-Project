87104849c724d3b97ab07305ca83d4ab911f2584
Lance009 <88927806+Lance0567@users.noreply.github.com>
2024-06-12 16:12:28 +0800

    button with external css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Card from "./Card";
+// How to Style REACT components with CSS
+// --------------------------------------
+// (not including external frameworks or processors)
+
+import Button from "./Button";
+
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
 
 
 function App() {
   return(
-    <>
-      <Card/>
-      <Card/>
-      <Card/>
-    </>
+    <Button/>
   );
 }
 
