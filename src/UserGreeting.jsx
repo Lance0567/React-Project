590a1b30f4acfbfe1bb2838ad322c78070ed8af2
Lance009 <88927806+Lance0567@users.noreply.github.com>
2024-06-12 18:48:04 +0800

    ternary operator

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -1,12 +1,8 @@
 
 function UserGreeting(props){
 
-    if(props.isLoggedIn){
-        return <h2>Welcome {props.username}</h2>
-    }
-    else {
-        return <h2>Please login to continue</h2>
-    }
+    return(props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.username}</h2> :
+                              <h2 className="login-prompt">Please log in to continue</h2>);
 }
 
 export default UserGreeting
